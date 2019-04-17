632eb92105b7bb4d1b4b6f8e3a93f4c4c4b00a26
Thijs De jong <tde-jong@f1r6s11.codam.nl>
2019-04-17 12:17:34 +0200

    fix 42filechecker

--- ft_ls.h
+++ ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: tde-jong <tde-jong@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/03/29 18:08:34 by tde-jong       #+#    #+#                */
-/*   Updated: 2019/04/17 11:32:00 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/04/17 12:14:42 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,14 @@
 # include <sys/xattr.h>
 # include <sys/ioctl.h>
 
-# define OPTIONS "RalrtS"
+# define OPTIONS "RalrtS1"
 # define OPT_R_UPP	(1 << 0)
 # define OPT_A		(1 << 1)
 # define OPT_L		(1 << 2)
 # define OPT_R		(1 << 3)
 # define OPT_T		(1 << 4)
 # define OPT_S_UPP	(1 << 5)
+# define OPT_ONE	(1 << 6)
 
 enum
 {
@@ -50,7 +51,6 @@ typedef struct stat		t_stat;
 typedef struct dirent	t_dirent;
 typedef struct passwd	t_passwd;
 typedef struct group	t_group;
-
 
 typedef struct			s_file
 {
